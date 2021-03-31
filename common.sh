5856733b2917de30fa4171871ca3d22005a0f3b2
Ashlen <eurydice@riseup.net>
2021-03-31 16:01:29 -0600

    bkup: double quotes changed to singles
    common.sh: today() is now a one-liner

--- common.sh
+++ common.sh
@@ -30,9 +30,7 @@ must_be_root() {
 }
 
 # print date in yyyy-mm-dd format.
-today() {
-  date '+%Y-%m-%d'
-}
+today() { date '+%Y-%m-%d' ;}
 
 # if the number of arguments isn't equal to the number needed, print the
 # usage details to STDERR and exit.
