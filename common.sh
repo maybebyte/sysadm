65de1110bda60923a9728228fb5a3e1abc684876
Ashlen <eurydice@riseup.net>
2022-04-01 18:06:17 -0600

    indentation via spaces and code alignment purge

--- common.sh
+++ common.sh
@@ -8,10 +8,10 @@
 # elevation, one is assigned to the alias 'priv.' doas is preferred over
 # sudo.
 if [ -x "$(command -v 'doas')" ]; then
-  alias priv='doas '
+	alias priv='doas '
 
 elif [ -x "$(command -v 'sudo')" ]; then
-  alias priv='sudo '
+	alias priv='sudo '
 
 fi
 
@@ -21,12 +21,12 @@ fi
 #
 # note that I only check executables that aren't accounted for in dotfiles.
 check_deps() {
-  while read -r dependency; do
+	while read -r dependency; do
 
-    [ -x "$(command -v -- "${dependency}")" ] \
-      || err "${dependency} not found in PATH or not executable."
+		[ -x "$(command -v -- "${dependency}")" ] \
+			|| err "${dependency} not found in PATH or not executable."
 
-  done
+	done
 }
 
 
@@ -36,12 +36,12 @@ check_deps() {
 # print everything passed as an argument.
 # exit with a return code of 1.
 err() {
-  printf '%s\n' "$*" >&2
-  exit 1
+	printf '%s\n' "$*" >&2
+	exit 1
 }
 
 
 # if the user isn't root, print an error message and exit.
 must_be_root() {
-  [ "$(id -u)" -eq 0 ] || err "Execute ${0##*/} with root privileges."
+	[ "$(id -u)" -eq 0 ] || err "Execute ${0##*/} with root privileges."
 }
--- genblock.pl
+++ genblock.pl
@@ -22,7 +22,7 @@ our $opt_t = 'plain';
 
 
 sub usage {
-  die <<EOT;
+	die <<EOT;
 $0 generates blocklists.
 
 usage: $0 [-h] [-o outfile] [-t type] [file (optional)] ...
@@ -41,55 +41,55 @@ EOT
 
 
 sub uniq {
-  my %seen;
-  return grep {! $seen{$_}++} @_;
+	my %seen;
+	return grep {! $seen{$_}++} @_;
 }
 
 
 sub format_blocklist {
-  # https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch08s15.html
-  #
-  # Somewhat malformed domain names (hyphens, underscores, uppercase,
-  # and numbers in places they shouldn't be) are accepted to catch those
-  # using resolvable out-of-spec domain names to evade regular
-  # expressions.
-  my $domain_regexp = '\b((?=[\w-]+\.)[\w-]+([\w-]+)*\.)+[\w-]+\b';
-  my @domains;
+	# https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch08s15.html
+	#
+	# Somewhat malformed domain names (hyphens, underscores, uppercase,
+	# and numbers in places they shouldn't be) are accepted to catch those
+	# using resolvable out-of-spec domain names to evade regular
+	# expressions.
+	my $domain_regexp = '\b((?=[\w-]+\.)[\w-]+([\w-]+)*\.)+[\w-]+\b';
+	my @domains;
 
-  while (<>) {
-    # Don't process commented or blank lines.
-    next if m/^(#|$)/;
-    # Fixes bogus entries like "0.0.0.0adobeflashplayerb.xyz" that
-    # will technically match $domain_regexp. We want to do this
-    # *before* the match, as "$&" entirely depends on what's matched.
-    s/127\.0\.0\.1|0\.0\.0\.0//g;
+	while (<>) {
+		# Don't process commented or blank lines.
+		next if m/^(#|$)/;
+		# Fixes bogus entries like "0.0.0.0adobeflashplayerb.xyz" that
+		# will technically match $domain_regexp. We want to do this
+		# *before* the match, as "$&" entirely depends on what's matched.
+		s/127\.0\.0\.1|0\.0\.0\.0//g;
 
-    if (m/$domain_regexp/) {
-      # If there are only integers and dots in the match, don't count
-      # it as a valid domain.
-      #
-      # This is needed since our domain regexp was necessarily bent to
-      # catch ne'er-do-wells, and it has lost some sanity as a result.
-      next if $& =~ m/^[\d\.]+$/;
+		if (m/$domain_regexp/) {
+			# If there are only integers and dots in the match, don't count
+			# it as a valid domain.
+			#
+			# This is needed since our domain regexp was necessarily bent to
+			# catch ne'er-do-wells, and it has lost some sanity as a result.
+			next if $& =~ m/^[\d\.]+$/;
 
-      # Convert any accepted uppercase to lowercase, since DNS is
-      # case-insensitive anyway.
-      $domains[++$#domains] = lc $&;
-    }
-  }
+			# Convert any accepted uppercase to lowercase, since DNS is
+			# case-insensitive anyway.
+			$domains[++$#domains] = lc $&;
+		}
+	}
 
-  my @unique_domains = uniq @domains;
+	my @unique_domains = uniq @domains;
 
-  if ($opt_t =~ m/^plain$/) {
-    # https://support.mozilla.org/en-US/kb/canary-domain-use-application-dnsnet
-    print "use-application-dns.net\n";
-    print "$_\n" for sort values @unique_domains;
-  }
+	if ($opt_t =~ m/^plain$/) {
+		# https://support.mozilla.org/en-US/kb/canary-domain-use-application-dnsnet
+		print "use-application-dns.net\n";
+		print "$_\n" for sort values @unique_domains;
+	}
 
-  elsif ($opt_t =~ m/^unbound$/) {
-    print "local-zone: \"use-application-dns.net\" always_refuse\n";
-    print "local-zone: \"$_\" always_refuse\n" for sort values @unique_domains;
-  }
+	elsif ($opt_t =~ m/^unbound$/) {
+		print "local-zone: \"use-application-dns.net\" always_refuse\n";
+		print "local-zone: \"$_\" always_refuse\n" for sort values @unique_domains;
+	}
 }
 
 
@@ -100,12 +100,12 @@ usage if $opt_h;
 die "$opt_t is not a valid type." if $opt_t !~ m/^(plain|unbound)$/;
 
 if ($opt_o) {
-  open my $fh, '>', $opt_o or die "Couldn't open $opt_o for writing.";
-  select $fh;
-  format_blocklist;
-  close $fh;
+	open my $fh, '>', $opt_o or die "Couldn't open $opt_o for writing.";
+	select $fh;
+	format_blocklist;
+	close $fh;
 }
 
 else {
-  format_blocklist;
+	format_blocklist;
 }
